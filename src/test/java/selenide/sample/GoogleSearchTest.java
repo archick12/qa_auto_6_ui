bddcc103d4b79ed0da37203d4c2b0f9dfdb686e7
apiliuk <lohika.artur@98point6.com>
2018-08-07 11:14:14 +0300

    initial commit

---
+++ src/test/java/selenide/sample/GoogleSearchTest.java
@@ -0,0 +1,22 @@
+package selenide.sample;
+
+import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GoogleSearchTest {
+  @Test
+  public void userCanSearchAnyKeyword() {
+
+    Configuration.browser="chrome";
+
+    open("http://google.com");
+    $(By.name("q")).val("selenide").pressEnter();
+    $$("#ires .g").shouldHave(size(10));
+    $("#ires .g").shouldHave(text("selenide.org"));
+  }
+}
