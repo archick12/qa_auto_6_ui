02b57ebdf3e3654bdc5a5f3ed6dcf3b6ce2448d0
vadim <vadlvadl789@gmail.com>
2018-08-20 00:09:04 +0300

    [newIssueTest] createNewIssue test added

--- src/test/java/tests/newIssueTest.java
+++ src/test/java/tests/newIssueTest.java
@@ -4,8 +4,11 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.DashboardPage;
 import pages.LoginPage;
+import pages.NewIssuePage;
 
 public class newIssueTest {
+
+    private String issueKey = "";
 
     @BeforeTest
     public void setup(){
@@ -23,6 +26,17 @@ public class newIssueTest {
     public void createNewIssueTest(){
 
         DashboardPage dashboardPage = new DashboardPage();
+        dashboardPage.navigate();
+        dashboardPage.atRequiredPage();
         dashboardPage.clickCreateIssueButton();
+
+        NewIssuePage newIssuePage = new NewIssuePage();
+        newIssuePage.atRequiredPage();
+        newIssuePage.enterSummary("[Test Automation] QAAUTO6-T1_testing_issue (eagles)");
+        newIssuePage.enterDescription("Testing issue created according to http://jira.hillel.it:8080/browse/QAAUT6-131 task");
+        newIssuePage.clickAssignToMe();
+        newIssuePage.clickCreateButton();
+
+
     }
 }
