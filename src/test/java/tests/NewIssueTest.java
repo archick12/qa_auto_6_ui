d20ae20b075895acda62772993e0b2ae0fa2ba82
Vadim Lizogub <vadlvadl789@gmail.com>
2018-08-20 19:41:31 +0300

    newIssueTest renamed to NewIssueTest

--- src/test/java/tests/NewIssueTest.java
+++ src/test/java/tests/NewIssueTest.java
@@ -1,24 +1,62 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import pages.DashboardPage;
-import pages.NewIssuePage;
-
-import static com.codeborne.selenide.Selenide.open;
+import pages.*;
 
 public class NewIssueTest {
 
-    @Test
-    public void addNewIssue(){
+    private String issueKey = "";
+    private String issueURL = "";
+
+    @BeforeTest
+    public void setup(){
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.navigate();
+        loginPage.atRequiredPage();
+        loginPage.enterLogin("webinar5");
+        loginPage.enterPassword("webinar5");
+        loginPage.clickSubmitButton();
+
+    }
+
+    @Test (priority = 1)
+    public void createNewIssueTest(){
 
         DashboardPage dashboardPage = new DashboardPage();
-        open("http://jira.hillel.it:8080/secure/Dashboard.jspa");
+        dashboardPage.navigate();
+        dashboardPage.atRequiredPage();
         dashboardPage.clickCreateIssueButton();
 
         NewIssuePage newIssuePage = new NewIssuePage();
-        newIssuePage.enterSummary("QAAUTO6_T1_test03");
-        newIssuePage.enterDescription("Testing Issue. Project QAAUTO6 Team1");
+        newIssuePage.atRequiredPage();
+        newIssuePage.enterSummary("[Test Automation] QAAUTO6-T1_testing_issue (eagles)");
+        newIssuePage.enterDescription("Testing issue created according to http://jira.hillel.it:8080/browse/QAAUT6-131 task");
         newIssuePage.clickAssignToMe();
         newIssuePage.clickCreateButton();
+
+        NotificationDialog dialog = new NotificationDialog();
+        issueKey = dialog.getCreatedIssueKey();
+        issueURL = dialog.getCreatedIssueLink();
+
+        System.out.println("Issue " + issueKey + " created \nURL: " + issueURL + "\n");
+
+        Assert.assertTrue(dialog.isSuccessDialogDisplayed());
+    }
+
+    @Test (priority = 2)
+    public void deleteCreatedIssue(){
+
+        IssuePage issuePage = new IssuePage();
+        issuePage.navigateTo(issueURL);
+        issuePage.atRequiredPage();
+        issuePage.clickMenuMoreButton();
+        issuePage.clickDeleteIssueButton();
+        issuePage.confirmDeleteIssue();
+
+        NotificationDialog dialog = new NotificationDialog();
+        Assert.assertTrue(dialog.isSuccessDialogDisplayed());
     }
 }
