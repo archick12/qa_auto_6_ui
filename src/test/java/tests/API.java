00ff70a7aa726ecb5c80b2a55a6e8a545d996861
SlavaKlepikov <40402515+SlavaKlepikov@users.noreply.github.com>
2018-09-27 12:09:14 +0300

    Add test create issue (#14)
    * added CreateIssueTest, added API test to testng.xml
    
    * change assigneeNamr to Weninar5

--- src/test/java/tests/API.java
+++ src/test/java/tests/API.java
@@ -3,6 +3,7 @@ package tests;
 import io.restassured.RestAssured;
 import io.restassured.response.ValidatableResponse;
 import org.json.simple.JSONObject;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
@@ -14,6 +15,7 @@ public class API {
     String sessionId;
     String issueId;
     String commentId;
+    String projectKey="QAAUT6";
 
     @BeforeSuite
     public void setupMethod(){
@@ -73,6 +75,59 @@ public class API {
                 post("/rest/auth/1/session").
                 then().
                 statusCode(401);
+    }
+
+    @Test
+    public void createIssueTest() {
+
+        String fieldsSummary = "The Issue was create via API test";
+        String issueTypeId = "10105";
+        String assigneeName = "Webinar5";
+
+        JSONObject issueCreate = new JSONObject();
+        JSONObject fields = new JSONObject();
+        JSONObject project = new JSONObject();
+        JSONObject issueType = new JSONObject();
+        JSONObject assignee = new JSONObject();
+
+        project.put("key", projectKey);
+        issueType.put("id", issueTypeId);
+        assignee.put("name", assigneeName);
+        fields.put("project", project);
+        fields.put("issuetype", issueType);
+        fields.put("assignee", assignee);
+        fields.put("summary", fieldsSummary);
+        issueCreate.put("fields", fields);
+
+        ValidatableResponse response = given().
+                header("Content-Type", "application/json").
+                header("Cookie", "JSESSIONID=" + sessionId).
+                body(issueCreate.toString()).
+                when().
+                post("/rest/api/2/issue").
+                then().
+                statusCode(201); // Code 201: Returns a link to the created issue.
+        issueId = response.extract().path("id");
+    }
+
+    @AfterTest
+    public void deleteIssueTest() {
+        ValidatableResponse responseDelete = given().
+                header("Content-Type", "application/json").
+                header("Cookie", "JSESSIONID=" + sessionId).
+                when().
+                delete("/rest/api/2/issue/" + issueId).
+                then().
+                statusCode(204);  //Code 204: Returned if the issue was successfully removed.
+
+        //Check that issue was delete and not found.
+                given().
+                header("Content-Type", "application/json").
+                header("Cookie", "JSESSIONID=" + sessionId).
+                when().
+                get("/rest/api/2/issue/" + issueId ).
+                then().
+                statusCode(404); //Code 404: Returned if the requested issue was not found, or the user does not have permission to view it.
     }
 
 }
